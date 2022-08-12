05ec6a6d3b42a4cb85fffdf5d4a1e0c72d535b08
triciachow0212 <suyeechow0212@gmail.com>
2022-08-12 16:01:41 +0800

    Create different routes and pages

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+export default function About() {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        Ad commodo sint sint ut do anim irure consequat est magna Lorem deserunt
+        ullamco. Labore magna amet cupidatat labore tempor aute nulla dolor. Qui
+        elit tempor incididunt consectetur ea laborum irure ex sunt laboris.
+      </p>
+    </div>
+  );
+}
---
+++ pages/ninjas/index.js
@@ -0,0 +1,7 @@
+export default function Ninjas() {
+  return (
+    <div>
+      <h1>All Ninjas</h1>
+    </div>
+  );
+}
